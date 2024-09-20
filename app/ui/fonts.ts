9de489bfcbc292b8f2de7ae29b87fe30d87b4936
Konstantin G <konstantinos3198@gmail.com>
2024-09-20 17:28:09 +0300

    first deployment

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import {Inter, Lusitana, Oxygen} from 'next/font/google';
+
+export const inter=Inter({subsets:['latin']});
+
+export const lusitana=Lusitana({subsets:['latin'],weight:["700","400"]});
+
+export const oxygen=Oxygen({subsets:['latin-ext'],weight:['700'],style:'normal',preload:true});
