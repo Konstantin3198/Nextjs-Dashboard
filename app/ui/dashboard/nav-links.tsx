bda881e88ea701198688abfe727507ae3cff1c8e
Konstantin G <konstantinos3198@gmail.com>
2024-09-20 17:48:16 +0300

    change Link to NextLink

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -5,7 +5,7 @@ import {
   HomeIcon, 
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-import Link from 'next/Link';
+import NextLink from 'next/link';
 import {usePathname} from 'next/navigation'
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -26,14 +26,14 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <Link
+          <NextLink
             key={link.name}
             href={link.href}
             className={`flex h-[48px] grow items-center ${link.href===pathname?`bg-sky-100 text-blue-600`:''} justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3`}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </Link>
+          </NextLink>
         );
       })}
     </>
